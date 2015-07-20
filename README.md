d38d11140e093fb3f17a85532456c8583e24776c
Tessa <symbiologer@gmail.com>
2015-07-20 12:31:46 -0400

    spark_test now runs DHT22 PietteTech example

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # ampiver
 SYDE 361 project - a smart grid sensor for ice accretion
 
-from the Spring 2015 offering of SYDE 361. Mostly planning to be code for the web UI and communication with the
-Particle Spark Core wifi development kit.
+from the Spring 2015 offering of SYDE 361. The project was a sensor to take the
+place of smart grid current sensors at various points in the electrical grid and
+broadcast temperature, humidity and ice presence. This allows grid management to
+be warned of and alerted to icing events.
+
+A UI mockup can be found on the gh-pages branch.
+Uses the [PipetteTech DHT Library](https://github.com/piettetech/PietteTech_DHT)
+to read DHT22 temperature and humidity values from the Spark Core.
--- spark_test.ino
+++ spark_test.ino
@@ -1,19 +1,93 @@
-int led1 = D0;
-int led2 = D7;
+// DHT22 Library from: https://github.com/pkourany/DHT22Spark/.
+#include "PietteTech_DHT.h"
 
-void setup() {
-   pinMode(led1, OUTPUT);
-   pinMode(led2, OUTPUT);
+#define DHTTYPE  DHT22       // Sensor type:  DHT22
+#define DHTPIN   7           // Digital pin for communications
+
+//declaration
+void dht_wrapper(); // must be declared before the lib initialization
+
+// Lib instantiate
+PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
+int n;      // counter
+
+
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial.available()) {
+        Serial.println("Press any key to start.");
+        delay (1000);
+    }
+    Serial.println("DHT Example program using DHT.acquireAndWait");
+    Serial.print("LIB version: ");
+    Serial.println(DHTLIB_VERSION);
+    Serial.println("---------------");
 }
 
-void loop() {
-   digitalWrite(led1, HIGH);
-   digitalWrite(led2, HIGH);
+// This wrapper is in charge of calling
+// must be defined like this for the lib work
+void dht_wrapper() {
+    DHT.isrCallback();
+}
 
-   delay(5000);
+void loop()
+{
+    Serial.print("\n");
+    Serial.print(n);
+    Serial.print(": Retrieving information from sensor: ");
+    Serial.print("Read sensor: ");
+    //delay(100);
 
-   digitalWrite(led1, LOW);
-   digitalWrite(led2, LOW);
+    int result = DHT.acquireAndWait();
 
-   delay(5000);
+    switch (result) {
+        case DHTLIB_OK:
+            Serial.println("OK");
+            break;
+        case DHTLIB_ERROR_CHECKSUM:
+            Serial.println("Error\n\r\tChecksum error");
+            break;
+        case DHTLIB_ERROR_ISR_TIMEOUT:
+            Serial.println("Error\n\r\tISR time out error");
+            break;
+        case DHTLIB_ERROR_RESPONSE_TIMEOUT:
+            Serial.println("Error\n\r\tResponse time out error");
+            break;
+        case DHTLIB_ERROR_DATA_TIMEOUT:
+            Serial.println("Error\n\r\tData time out error");
+            break;
+        case DHTLIB_ERROR_ACQUIRING:
+            Serial.println("Error\n\r\tAcquiring");
+            break;
+        case DHTLIB_ERROR_DELTA:
+            Serial.println("Error\n\r\tDelta time to small");
+            break;
+        case DHTLIB_ERROR_NOTSTARTED:
+            Serial.println("Error\n\r\tNot started");
+            break;
+        default:
+            Serial.println("Unknown error");
+            break;
+    }
+    Serial.print("Humidity (%): ");
+    Serial.println(DHT.getHumidity(), 2);
+
+    Serial.print("Temperature (oC): ");
+    Serial.println(DHT.getCelsius(), 2);
+
+    Serial.print("Temperature (oF): ");
+    Serial.println(DHT.getFahrenheit(), 2);
+
+    Serial.print("Temperature (K): ");
+    Serial.println(DHT.getKelvin(), 2);
+
+    Serial.print("Dew Point (oC): ");
+    Serial.println(DHT.getDewPoint());
+
+    Serial.print("Dew Point Slow (oC): ");
+    Serial.println(DHT.getDewPointSlow());
+
+    n++;
+    delay(2500);
 }
