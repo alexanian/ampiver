51232950eb01857c00cb23910a91f4945b3d7759
Tessa <symbiologer@gmail.com>
2015-07-19 23:29:42 -0400

    LED Blinker as a first Spark Core test

---
+++ spark_test.ino
@@ -0,0 +1,19 @@
+int led1 = D0;
+int led2 = D7;
+
+void setup() {
+   pinMode(led1, OUTPUT);
+   pinMode(led2, OUTPUT);
+}
+
+void loop() {
+   digitalWrite(led1, HIGH);
+   digitalWrite(led2, HIGH);
+
+   delay(5000);
+
+   digitalWrite(led1, LOW);
+   digitalWrite(led2, LOW);
+
+   delay(5000);
+}
