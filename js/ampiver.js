eeef5724b9b056f1bf8c6f678a51b1c83c6f054c
Tessa <symbiologer@gmail.com>
2015-07-21 20:01:16 -0400

    Adds vex library for popups + status modes popup

---
+++ js/ampiver.js
@@ -0,0 +1,33 @@
+// Custom JS for Ampiver UI
+vex.defaultOptions.className = 'vex-theme-flat-attack';
+
+// Make status descriptions appear as overlay using Vex
+function showStatusDetails() {
+    vex.dialog.alert(
+    '<h3>Sensor Modes</h3>\n' +
+    '<div class="pull-left standby status-circle">' +
+    '<div class="status-container"><div class="status-content"><span class="glyphicon glyphicon-time" ' +
+    'aria-hidden="true"></span></div></div>\n    </div>' +
+    '<p><strong>Standby</strong> indicates that temperature and humidity are outside the range where there is risk' +
+    ' of ice accretion.</p> \n' +
+    '<div class="pull-left icing-risk status-circle">' +
+    '<div class="status-container"><div class="status-content"><span class="glyphicon glyphicon-tint" ' +
+    'aria-hidden="true"></span></div></div>\n    </div>' +
+    '<p><strong>Icing Risk</strong> indicates that temperature and humidity are in the range where there is risk of' +
+    ' ice accretion.</p> \n' +
+    '<p></p> \n' +
+    '<div class="pull-left icing-event status-circle">' +
+    '<div class="status-container"><div class="status-content"><span class="glyphicon glyphicon-asterisk" ' +
+    'aria-hidden="true"></span></div></div>\n    </div>' +
+    '<p><strong>Icing Event</strong> indicates that ice has been detected on the sensor. Time to begin de-icing' +
+    ' efforts!</p> \n' +
+    '<div class="pull-left no-current status-circle">' +
+    '<div class="status-container"><div class="status-content"><span class="glyphicon glyphicon-warning-sign" ' +
+    'aria-hidden="true"></span></div></div>\n    </div>' +
+    '<p><strong>No Current</strong> indicates that no current is being detected and the sensor is' +
+    ' running on reserve power. Check the station!</p> \n');
+}
+
+$(document).ready( function() {
+    $('#current-status').click(showStatusDetails);
+});
