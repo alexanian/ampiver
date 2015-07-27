fb711131fa93dc3ba1674f2044144dabd00f21a8
Tessa <symbiologer@gmail.com>
2015-07-27 18:50:49 -0400

    Implements modes, fixes centering of status glyphicons

--- js/ampiver-spark.js
+++ js/ampiver-spark.js
@@ -30,21 +30,23 @@ window.setInterval(function() {
 
 // Set mode - probably could be made more efficient
 function setSensorMode() {
-    curr = parseFloat($("#curr-readout").innerHTML);
-    tmpr = parseFloat($("#tmpr-readout").innerHTML);
-    console.log($("#curr-readout"));
+    curr = parseFloat($("#curr-readout").text());
+    tmpr = parseFloat($("#tmpr-readout").text());
     console.log(curr);
     console.log(tmpr);
 
     // Remove sensor status class
     $("#sensor-status").removeClass( "standby no-current icing-risk icing-event" )
     
-    if (curr < 0.05) { // no current mode
+    if (curr < 0.09) { // no current mode
         $("#sensor-status").addClass( "no-current" );
-        alert(curr);
+        $("#sensor-status").html('<div class="status-content"><span class="glyphicon glyphicon-warning-sign" aria-hidden="true"></div>');
+
     } else if (tmpr < -40.0 || tmpr > 0.0 ) { // standby mode - really ought to also check humidity
         $("#sensor-status").addClass( "standby" );
+        $("#sensor-status").html('<div class="status-content"><span class="glyphicon glyphicon-time" aria-hidden="true"></div>');
     } else { // icing-risk mode; icing-event not implemented since QCM does not transmit data
         $("#sensor-status").addClass( "icing-risk" );
+        $("#sensor-status").html('<div class="status-content"><span class="glyphicon glyphicon-tint" aria-hidden="true"></div>');
     }
 }
